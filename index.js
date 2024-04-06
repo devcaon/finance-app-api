2aa793c0768287d28744a4accb73bf57ab6cd201
Claudio Oliveira <dev.caon@gmail.com>
2024-04-06 12:54:20 -0300

    feat: add GetUserByIdController and validations

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
 import 'dotenv/config.js'
 import express from 'express'
 import { CreateUserController } from './src/controllers/create-user.js'
+import { GetUserByIdController } from './src/controllers/get-user-by-id.js'
 
 const app = express()
 app.use(express.json())
 
 const PORT = process.env.PORT || 5000
 
-// routes
+// User routes
 app.post('/api/users', async (req, res) => {
     const createUserController = new CreateUserController()
 
-    const response = await createUserController.execute(req)
+    const { statusCode, body } = await createUserController.execute(req)
 
-    res.status(response.statusCode).json(response.body)
+    res.status(statusCode).send(body)
+})
+
+app.get('/api/users/:userId', async (req, res) => {
+    const getUserByIdController = new GetUserByIdController()
+
+    const { statusCode, body } = await getUserByIdController.execute(req)
+
+    res.status(statusCode).send(body)
 })
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
