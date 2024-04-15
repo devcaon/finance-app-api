4c080996c260805326dfeacb4bace2b617925739
Claudio Oliveira <dev.caon@gmail.com>
2024-04-14 23:59:13 -0300

    refactor: move user use cases to its own folder

---
+++ src/use-cases/user/create-user.js
@@ -0,0 +1,36 @@
+import { v4 as uuidv4 } from 'uuid'
+import bcrypt from 'bcrypt'
+import { EmailAlreadyInUseError } from '../../errors/user.js'
+
+export class CreateUserUseCase {
+    constructor(getUserByEmailRepository, createUserRepository) {
+        this.getUserByEmailRepository = getUserByEmailRepository
+        this.createUserRepository = createUserRepository
+    }
+    async execute(createUserParams) {
+        const userWithProvidedEmail =
+            await this.getUserByEmailRepository.execute(createUserParams.email)
+
+        if (userWithProvidedEmail) {
+            throw new EmailAlreadyInUseError(createUserParams.email)
+        }
+
+        // Gerar ID do usuário
+        const userId = uuidv4()
+
+        // criptografar a senha
+        const hashedPassword = await bcrypt.hash(createUserParams.password, 10)
+
+        // inserir a senha no banco de dados
+        const user = {
+            ...createUserParams,
+            id: userId,
+            password: hashedPassword,
+        }
+
+        // chamar o repositório
+        const createdUser = await this.createUserRepository.execute(user)
+
+        return createdUser
+    }
+}
---
+++ src/use-cases/user/delete-user.js
@@ -0,0 +1,10 @@
+export class DeleteUserUseCase {
+    constructor(deleteUserRepository) {
+        this.deleteUserRepository = deleteUserRepository
+    }
+    async execute(userId) {
+        const deleteUser = await this.deleteUserRepository.execute(userId)
+
+        return deleteUser
+    }
+}
