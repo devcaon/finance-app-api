bedd4444f148600fa35bed2662cc0035f4247dfc
Claudio Oliveira <dev.caon@gmail.com>
2024-04-20 01:44:15 -0300

    feat: added DeleteTransactionController

--- src/controllers/transaction/delete-transaction.js
+++ src/controllers/transaction/delete-transaction.js
@@ -1,5 +1,31 @@
+import {
+    checkIfIdIsValid,
+    invalidIdResponse,
+    ok,
+    serverError,
+} from '../helpers/index.js'
+
 export class DeleteTransactionController {
     constructor(deleteTransactionUseCase) {
         this.deleteTransactionUseCase = deleteTransactionUseCase
+    }
+
+    async execute(httpRequest) {
+        try {
+            // verificar se id é valido
+            if (!checkIfIdIsValid(httpRequest.params.transactionId)) {
+                return invalidIdResponse()
+            }
+
+            // chama use case
+            const transaction = await this.deleteTransactionUseCase.execute(
+                httpRequest.params.transactionId,
+            )
+
+            return ok(transaction)
+        } catch (error) {
+            console.error(error)
+            return serverError()
+        }
     }
 }
