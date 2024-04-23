112a84fb6b1543032c1d539a4f953e58ab29d524
Claudio Oliveira <dev.caon@gmail.com>
2024-04-23 01:28:26 -0300

    feat: added GetUserBalanceUseCase

---
+++ src/use-cases/user/get-user-balance.js
@@ -0,0 +1,24 @@
+import { UserNotFoundError } from '../../errors/user.js'
+
+export class GetUserBalanceUseCase {
+    constructor(getUserBalanceRepository, getUserByIdRepository) {
+        this.getUserBalanceRepository = getUserBalanceRepository
+        this.getUserByIdRepository = getUserByIdRepository
+    }
+    async execute(params) {
+        // check if user exists
+        const user = await this.getUserByIdRepository.execute(params.userId)
+
+        if (!user) {
+            // if user not found
+            throw new UserNotFoundError()
+        }
+
+        // call user balance repository
+        const balance = await this.getUserBalanceRepository.execute(
+            params.userId,
+        )
+
+        return balance
+    }
+}
