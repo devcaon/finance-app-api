095cc1b8609dca311ffa6ebc5e28db3f85e658c1
Claudio Oliveira <dev.caon@gmail.com>
2024-04-23 23:59:32 -0300

    feat: added zod to CreateUserController

--- src/controllers/user/create-user.js
+++ src/controllers/user/create-user.js
@@ -1,15 +1,7 @@
 import { EmailAlreadyInUseError } from '../../errors/user.js'
-import {
-    checkIfEmailIsValid,
-    checkIfPasswordIsValid,
-    emailIsAlreadyInUseReponse,
-    invalidPasswordResponse,
-    badRequest,
-    created,
-    serverError,
-    validateRequiredFields,
-    requiredFieldsIsMissingResponse,
-} from '../helpers/index.js'
+import { badRequest, created, serverError } from '../helpers/index.js'
+import { ZodError } from 'zod'
+import { createUserSchema } from '../../schemas/index.js'
 
 export class CreateUserController {
     constructor(createUserUseCase) {
@@ -19,41 +11,18 @@ export class CreateUserController {
         try {
             const params = httpRequest.body
 
-            // validar a requisição (campos obrigatórios, tamanho de senha e e-mail)
-            const requiredFields = [
-                'first_name',
-                'last_name',
-                'email',
-                'password',
-            ]
+            // Validations
+            await createUserSchema.parseAsync(params)
 
-            const { ok, missingField } = validateRequiredFields(
-                params,
-                requiredFields,
-            )
+            const createdUser = await this.createUserUseCase.execute(params)
 
-            if (!ok) {
-                return requiredFieldsIsMissingResponse(missingField)
-            }
-
-            if (!checkIfPasswordIsValid(params.password)) {
-                return invalidPasswordResponse()
-            }
-
-            if (!checkIfEmailIsValid(params.email)) {
-                return emailIsAlreadyInUseReponse()
-            }
-
-            // chamar use case
-            // const createUserUseCase = new CreateUserUseCase()
-
-            const createdUser = await this.createUserUseCase.execute(
-                httpRequest.body,
-            )
-
-            // retornar a resposta para o usuário (status code)
             return created(createdUser)
         } catch (error) {
+            if (error instanceof ZodError) {
+                return badRequest({
+                    message: error.errors[0].message,
+                })
+            }
             if (error instanceof EmailAlreadyInUseError) {
                 return badRequest({ message: error.message })
             }
